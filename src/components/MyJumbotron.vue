1127718f21c1576b6f3a715d055c5ce123ecd6bf
Hugo <hugo.barnas@gmail.com>
2020-08-29 10:45:26 +0200

    part 2 Second Step : Router ==> OK

--- src/components/MyJumbotron.vue
+++ src/components/MyJumbotron.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="text-center">
-    <b-jumbotron header="Todo List" lead="New Features we will have to done for this project">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-navbar-brand href="http://localhost:8080">Todolist</b-navbar-brand>
+        <b-nav-item router-link to="/new">New</b-nav-item>
+        <b-nav-item router-link to="/todo">ToDo</b-nav-item>
+        <b-nav-item router-link to="/done">Done</b-nav-item>
+        <b-nav-item router-link to="/list">List</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-jumbotron
+      header="Todo List"
+      lead="New Features we will have to done for this project"
+    >
       <hr class="my-4" />
       <p>Easy to use, we created this web app just for you!</p>
       <sentence v-bind="{ ListD }"></sentence>
       <hr class="my-4" />
       <list @strike="test" v-bind="{ ListD }" />
-      </b-jumbotron>
-      <add v-on:task="task2"></add>
+    </b-jumbotron>
+    <add v-on:task="task2"></add>
   </div>
 </template>
-
 <script>
 import list from "./list";
 import add from "./AddForm";
@@ -26,9 +37,9 @@ export default {
     test: function(id) {
       this.$emit("strike", id);
     },
-    task2: function(y){
-      this.$emit("taskname2", y)
-    }
+    task2: function(y) {
+      this.$emit("taskname2", y);
+    },
   },
 };
 </script>
